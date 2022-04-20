c486fab0b24db7154899dbc54a9d1d4828f71efa
Hasan Akçay <104010801+Hasaanakcay@users.noreply.github.com>
2022-04-20 08:52:03 +0300

    C++ İle banka uygulaması
    TCP Server ve TCP Client olarak iki konsol uygulaması geliştirilecek olup,
    Server'da önceden oluşturulmuş farklı bankalara ait kullanıcı hesapları, kartları ve
    hesaplara ait bakiyeler (herhangi bir .txt dosyasına kaydedilmiş olabilir, yada database
    kullanılabilir.) üzerinde işlemler server tarafında yapılacaktır.
    TCP server aracılığı Client'tan gelen isteğe göre para transferi gerçekleştirilecektir.
    Kullanıcılar Client uygulamasında kullanıcı adı ve parola ile giriş yapıp bakiyesini
    görebilecek ve para göndermek için sistemde kayıtlı olan diğer müşteri numarasını,
    isim-soyadını ve göndereceği miktarı yazarak yeterli bakiye varsa belirlenen kullanıcıya
    transfer yapılacaktır. Eğer yeterli bakiye yok ise işlem gerçekleştirilemeyecektir. Ayrıca
    kendi bakiyesinden para da çekebilecektir ve bakiyesine para yatırabilecektir. Bir
    hesaptan diğer hesaba transfer yapılırken eğer transfer yapılan hesap farklı bankaya
    ait ise önceden belirlenmiş miktar kadar kesinti yapılacaktır.

---
+++ Server/Server/Server.cpp
@@ -0,0 +1,246 @@
+﻿#include <iostream>
+#include <WS2tcpip.h>
+#include <string>
+#include <fstream>
+#include <sstream>  
+
+#pragma comment (lib, "ws2_32.lib")
+
+using namespace std;
+
+void main()
+{
+	int userNumber = 6 * 5; //  kullanıcı sayısına göre güncellenir.
+	string kullanicilar[30]; 
+	string satir;
+	ifstream okumadosyası;
+	okumadosyası.open("kullanicilar.txt");
+	for (int i = 0; i < userNumber; i++) {
+		getline(okumadosyası, satir);
+		kullanicilar[i] = satir;
+		cout << kullanicilar[i] << endl;
+	}
+	int kullanıcıID;
+
+	
+	WSADATA wsData;
+	WORD ver = MAKEWORD(2, 2);
+
+	int wsOk = WSAStartup(ver, &wsData);
+	if (wsOk != 0)
+	{
+		cerr << "Winsock baslatilamiyor!" << endl;
+		return;
+	}
+
+	SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
+	if (listening == INVALID_SOCKET)
+	{
+		cerr << "Soket olusturulamiyor! " << endl;
+		return;
+	}
+
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(54000);
+	hint.sin_addr.S_un.S_addr = INADDR_ANY; 
+
+	bind(listening, (sockaddr*)&hint, sizeof(hint));
+
+	listen(listening, SOMAXCONN);
+
+	sockaddr_in client;
+	int clientSize = sizeof(client);
+
+	SOCKET clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
+
+	char host[NI_MAXHOST];		
+	char service[NI_MAXSERV];	
+
+	ZeroMemory(host, NI_MAXHOST); 
+	ZeroMemory(service, NI_MAXSERV);
+
+	if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
+	{
+		cout << host << " connected on port " << service << endl;
+	}
+	else
+	{
+		inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+		cout << host << " connected on port " <<
+			ntohs(client.sin_port) << endl;
+	}
+
+	closesocket(listening);
+
+	
+	char buf[4096];
+	int kontrol = 0;
+	while (kontrol == 0)
+	{
+		ZeroMemory(buf, 4096);
+
+		
+		int bytesReceived = recv(clientSocket, buf, 4096, 0);
+		string username = string(buf, 0, bytesReceived);
+
+		int i = 0;
+		while (i < userNumber && kontrol == 0) {
+			if (username == kullanicilar[i]) {
+				send(clientSocket, "1", 4096, 0);
+				kontrol = 1;
+				kullanıcıID = i;
+			}
+			i += 5;
+		}
+
+		if (kontrol == 0) {
+			send(clientSocket, "0", 4096, 0);
+		}
+
+
+	}
+	kontrol = 0;
+	while (kontrol == 0)
+	{
+		ZeroMemory(buf, 4096);
+
+		int bytesReceived = recv(clientSocket, buf, 4096, 0);
+		string pass = string(buf, 0, bytesReceived);
+
+		if (pass == kullanicilar[kullanıcıID + 1]) {
+			string metin = kullanicilar[kullanıcıID + 2] + " - " + kullanicilar[kullanıcıID + 3] + " - " + kullanicilar[kullanıcıID + 4];
+			send(clientSocket, metin.c_str(), 4096, 0);
+			kontrol = 1;
+		}
+		else {
+			send(clientSocket, "0", 4096, 0);
+		}
+
+
+	}
+	kontrol = 0;
+	while (kontrol == 0)
+	{
+		ZeroMemory(buf, 4096);
+
+		int bytesReceived = recv(clientSocket, buf, 4096, 0);
+		string TrNum = string(buf, 0, bytesReceived);
+
+		if (TrNum == "1") {
+
+			ZeroMemory(buf, 4096);
+			int bytesReceived = recv(clientSocket, buf, 4096, 0);
+			string amount = string(buf, 0, bytesReceived);
+			float bakiye = std::stof(kullanicilar[kullanıcıID + 4]);
+			bakiye = std::stof(amount) + std::stof(kullanicilar[kullanıcıID + 4]);
+			kullanicilar[kullanıcıID + 4] = std::to_string(bakiye);
+			remove("kullanicilar.txt");
+			ofstream yarat("kullanicilar.txt");
+			for (size_t i = 0; i < userNumber; i++)
+			{
+				yarat << kullanicilar[i] << endl;
+			}
+			string metin = "Para Yatirma Basarili. Yeni Hesap :" + kullanicilar[kullanıcıID + 4];
+			send(clientSocket, metin.c_str(), 4096, 0);
+
+		}
+
+		else if (TrNum == "2") {
+
+			ZeroMemory(buf, 4096);
+			int bytesReceived = recv(clientSocket, buf, 4096, 0);
+			string amount = string(buf, 0, bytesReceived);
+			float bakiye = std::stof(kullanicilar[kullanıcıID + 4]);
+			if (std::stof(amount) > bakiye) {
+				send(clientSocket, "Yetersiz bakiye! Lutfen Hesabinizi Kontrol Edin.", 4096, 0);
+			}
+			else
+			{
+				bakiye = std::stof(kullanicilar[kullanıcıID + 4]) - std::stof(amount);
+				kullanicilar[kullanıcıID + 4] = std::to_string(bakiye);
+				remove("kullanicilar.txt");
+				ofstream yarat("kullanicilar.txt");
+				for (size_t i = 0; i < userNumber; i++)
+				{
+					yarat << kullanicilar[i] << endl;
+				}
+				string metin = "Geri Cekilme Basarili. Yeni Hesap : " + kullanicilar[kullanıcıID + 4];
+				send(clientSocket, metin.c_str(), 4096, 0);
+
+			}
+
+		}
+
+		else if (TrNum == "3") {
+			int alici = 0;
+			int k = 3;
+			int kesinti = 0;
+			ZeroMemory(buf, 4096);
+			int bytesReceived = recv(clientSocket, buf, 4096, 0);
+			string accnNum = string(buf, 0, bytesReceived);
+			while (k < userNumber && alici == 0)
+			{
+				if (accnNum == kullanicilar[k]) {
+					alici = k;
+				}
+				else
+				{
+					k += 5;
+				}
+
+			}
+			if (alici != 0) {
+				send(clientSocket, "1", 4096, 0);
+				ZeroMemory(buf, 4096);
+				int bytesReceived = recv(clientSocket, buf, 4096, 0);
+				string amount = string(buf, 0, bytesReceived);
+				float bakiye = std::stof(kullanicilar[kullanıcıID + 4]);
+				if (kullanicilar[kullanıcıID + 2] != kullanicilar[k - 1])
+				{
+					kesinti = 5;
+				}
+
+				if ((std::stof(amount)) + kesinti > bakiye) {
+					send(clientSocket, "Yetersiz bakiye! Lutfen Hesabinizi Kontrol Edin.", 4096, 0);
+				}
+				else
+				{
+
+					bakiye = std::stof(kullanicilar[kullanıcıID + 4]) - (std::stof(amount) + kesinti);
+					kullanicilar[kullanıcıID + 4] = std::to_string(bakiye);
+					float gonderilenbakiye = std::stof(kullanicilar[k + 1]);
+					gonderilenbakiye = std::stof(kullanicilar[k + 1]) + std::stof(amount);
+					kullanicilar[k + 1] = std::to_string(gonderilenbakiye);
+					remove("kullanicilar.txt");
+					ofstream yarat("kullanicilar.txt");
+					for (size_t i = 0; i < userNumber; i++)
+					{
+						yarat << kullanicilar[i] << endl;
+					}
+					string metin = "Para Gonder Islemi Basarili. Yeni Hesap : " + kullanicilar[kullanıcıID + 4  ];
+					send(clientSocket, metin.c_str(), 4096, 0);
+
+				}
+			}
+			else
+			{
+				send(clientSocket, "Hesap bulunamadi! Lutfen tekrar deneyin.", 4096, 0);
+			}
+
+		}
+
+		else if (TrNum == "4") {
+			kontrol = 1;
+
+		}
+
+	}
+	// Close the socket
+	closesocket(clientSocket);
+
+	// Cleanup winsock
+	WSACleanup();
+
+	system("pause");
+}
---
+++ client/client/client.cpp
@@ -0,0 +1,198 @@
+﻿#include <iostream>
+#include <string>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+using namespace std;
+
+void main()
+{
+	string ipAddress = "127.0.0.1";			
+	int port = 54000;						
+
+	// Initialize WinSock
+	WSAData data;
+	WORD ver = MAKEWORD(2, 2);
+	int wsResult = WSAStartup(ver, &data);
+	if (wsResult != 0)
+	{
+		cerr << "Winsock baslatılamiyor, Err #" << wsResult << endl;
+		return;
+	}
+
+	// Create socket
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == INVALID_SOCKET)
+	{
+		cerr << "Soket olusturulamiyor, Err #" << WSAGetLastError() << endl;
+		WSACleanup();
+		return;
+	}
+
+	// Fill in a hint structure
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(port);
+	inet_pton(AF_INET, ipAddress.c_str(), &hint.sin_addr);
+
+	// Connect to server
+	int connResult = connect(sock, (sockaddr*)&hint, sizeof(hint));
+	if (connResult == SOCKET_ERROR)
+	{
+		cerr << "Sunucuya baglanilamiyor, Err #" << WSAGetLastError() << endl;
+		closesocket(sock);
+		WSACleanup();
+		return;
+	}
+
+	
+	char buf[4096];
+	string userInput;
+	string amount;
+	string accNum;
+	cout << "TCP Banka Sistemi " << endl;
+	int kontrol = 0;
+
+	do
+	{
+
+		cout << "Lutfen Isminizi giriniz : ";
+		getline(cin, userInput);
+
+
+		if (userInput.size() > 0)		
+		{
+			// Send the text
+			int sendResult = send(sock, userInput.c_str(), userInput.size() + 1, 0);
+			if (sendResult != SOCKET_ERROR)
+			{
+				// Wait for response
+				ZeroMemory(buf, 4096);
+				int bytesReceived = recv(sock, buf, 4096, 0);
+				if (bytesReceived > 0)
+				{
+
+					if (string(buf, 0, bytesReceived) == "1") {
+						kontrol = 1;
+						cout << "Sifre Girme Adimina Yonlendiriliyorsunuz..." << endl;
+					}
+					else {
+						cout << "Gecersiz kullanici adı. Tekrar deneyin." << endl;
+					}
+				}
+			}
+		}
+
+	} while (kontrol == 0);
+	kontrol = 0;
+	do
+	{
+		cout << "Lutfen Kullanici Sifrenizi Giriniz : ";
+		getline(cin, userInput);
+
+
+
+		if (userInput.size() > 0)
+		{
+
+			int sendResult = send(sock, userInput.c_str(), userInput.size() + 1, 0);
+			if (sendResult != SOCKET_ERROR)
+			{
+
+				ZeroMemory(buf, 4096);
+				int bytesReceived = recv(sock, buf, 4096, 0);
+				if (bytesReceived > 0)
+				{
+
+					if (string(buf, 0, bytesReceived) != "0") {
+						cout << "Giris basarili. Lutfen bekleyin ..." << endl;
+						kontrol = 1;
+						cout << "HOSGELDINIZ" << endl;
+						cout << " Banka Adi - Hesap Numarasi - Bakiye " << endl;
+						cout << string(buf, 0, bytesReceived) << endl;
+					}
+					else {
+						cout << "Yanlıs sifre.Tekrar deneyin." << endl;
+					}
+				}
+			}
+		}
+
+	} while (kontrol == 0);
+	kontrol = 0;
+	do
+	{
+
+		cout << "1- Hesabiniza Para Yatirma> " << endl;
+		cout << "2- Hesabinizdan Para Cekme > " << endl;
+		cout << "3- Baska Bir Hesaba Para Transferi > " << endl;
+		cout << "4- CIKIS > " << endl;
+		cout << " Lutfen yapmak istediginiz islem numarasini giriniz : ";
+		getline(cin, userInput);
+		int sendResult = send(sock, userInput.c_str(), userInput.size() + 1, 0);
+		if (userInput == "1")
+		{
+			cout << "Yatirilacak Tutari Girin :  ";
+			getline(cin, amount);
+			int sendResult = send(sock, amount.c_str(), amount.size() + 1, 0);
+			ZeroMemory(buf, 4096);
+			int bytesReceived = recv(sock, buf, 4096, 0);
+			if (bytesReceived > 0)
+			{
+				cout << "SERVER> " << string(buf, 0, bytesReceived) << endl;
+
+			}
+		}
+		else if (userInput == "2")
+		{
+			cout << "Cekilecek Tutari Girin : ";
+			getline(cin, amount);
+			int sendResult = send(sock, amount.c_str(), amount.size() + 1, 0);
+			ZeroMemory(buf, 4096);
+			int bytesReceived = recv(sock, buf, 4096, 0);
+			if (bytesReceived > 0)
+			{
+				cout << "SERVER> " << string(buf, 0, bytesReceived) << endl;
+
+			}
+		}
+		else if (userInput == "3")
+		{
+			cout << "Para Gonderilecek Hesap Numarasini Girin : ";
+			getline(cin, accNum);
+			int sendResult = send(sock, accNum.c_str(), accNum.size() + 1, 0);
+			ZeroMemory(buf, 4096);
+			int bytesReceived = recv(sock, buf, 4096, 0);
+			if (bytesReceived > 0)
+			{
+				if (string(buf, 0, bytesReceived) == "1")
+				{
+					cout << "Dikkat! Baska Bir Bankaya Havale Yapiyorsaniz 5 TL EFT Ucreti Alinacaktir.";
+					cout << "Gonderilecek Para Miktarini Giriniz : ";
+					getline(cin, amount);
+					int sendResult = send(sock, amount.c_str(), amount.size() + 1, 0);
+					ZeroMemory(buf, 4096);
+					int bytesReceived = recv(sock, buf, 4096, 0);
+					if (bytesReceived > 0)
+					{
+						cout << "SERVER> " << string(buf, 0, bytesReceived) << endl;
+
+					}
+				}
+				else
+				{
+					cout << "SERVER> " << string(buf, 0, bytesReceived) << endl;
+				}
+
+			}
+		}
+		else if (userInput == "4") {
+			kontrol = 1;
+		}
+
+	} while (kontrol == 0);
+
+	
+	closesocket(sock);
+	WSACleanup();
+}
